38e9fcb92341fb2dfb9f458bfaa2dde580feed2f
unavailable101 <144946995+unavailable101@users.noreply.github.com>
2024-03-08 10:25:41 +0800

    for sql

---
+++ dbcatubigf2.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 08, 2024 at 03:26 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbcatubigf2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluseraccount`
+--
+
+CREATE TABLE `tbluseraccount` (
+  `acctid` int(5) NOT NULL,
+  `emailadd` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `usertype` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbluseraccount`
+--
+
+INSERT INTO `tbluseraccount` (`acctid`, `emailadd`, `username`, `password`, `usertype`) VALUES
+(1, 'nina@gmail.com', 'ninx', '123456', ''),
+(2, 'maku@gmail.com', 'raku', '123456', ''),
+(3, 'a@gmail.com', 'c', '123456', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluserprofile`
+--
+
+CREATE TABLE `tbluserprofile` (
+  `userid` int(5) NOT NULL,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbluserprofile`
+--
+
+INSERT INTO `tbluserprofile` (`userid`, `firstname`, `lastname`, `gender`) VALUES
+(1, 'nina', 'catubig', 'female'),
+(2, 'nina', 'catubig', 'female'),
+(3, 'marha', 'catis', 'male'),
+(4, 'a', 'b', 'female');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbluseraccount`
+--
+ALTER TABLE `tbluseraccount`
+  ADD PRIMARY KEY (`acctid`);
+
+--
+-- Indexes for table `tbluserprofile`
+--
+ALTER TABLE `tbluserprofile`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbluseraccount`
+--
+ALTER TABLE `tbluseraccount`
+  MODIFY `acctid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbluserprofile`
+--
+ALTER TABLE `tbluserprofile`
+  MODIFY `userid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
